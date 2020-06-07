1f856671bd6bfb691bbc364993eac2abfdc1de02
Mohammad Hossein Ahmadi <mh.ahmadi258.work@gmail.com>
2020-06-08 04:13:47 +0430

    change phone filed to null = True

--- authapp/models.py
+++ authapp/models.py
@@ -14,15 +14,17 @@ def user_image_upload_location(instance, filename):
 
 
 class User(AbstractUser):
-    email = models.EmailField('email address', unique=True, blank=False)
-    phone = models.CharField('phone number', max_length = 11,validators=(
-        phone_validator,), blank=True, null=False , unique=True)
+    email = models.EmailField(
+        'email address', unique=True, null=False, blank=False)
+    phone = models.CharField('phone number', max_length=11, validators=(
+        phone_validator,), blank=True, null=True, unique=True)
     image = models.ImageField(
         'profile photo', blank=True, null=True, upload_to=user_image_upload_location)
     birth_date = models.DateField('birth date', blank=True, null=True)
     city = models.ForeignKey('City', related_name='user',
                              on_delete=models.SET_NULL, blank=True, null=True)
-    address = models.TextField('address',validators=(MaxLengthValidator(256),), blank=True)
+    address = models.TextField('address', validators=(
+        MaxLengthValidator(256),), blank=True, null=False)
 
     def __str__(self):
         return self.username
@@ -32,7 +34,7 @@ class User(AbstractUser):
 
 
 class City(models.Model):
-    name = models.CharField('city name' , max_length = 30)
+    name = models.CharField('city name', max_length=30)
 
     def __str__(self):
         return self.name
