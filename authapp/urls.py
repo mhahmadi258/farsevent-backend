a7b52c48073a102ccd585e0869c958c95b1aefd0
Mohammad Hossein Ahmadi <mh.ahmadi258.work@gmail.com>
2020-06-21 06:28:50 +0430

    add login feature

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import UserCreationView
+from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'authapp'
 
 urlpatterns = [
     path('create/',UserCreationView.as_view(),name='user creation'),
+    path('login/',obtain_auth_token, name = 'login'),
 ]
