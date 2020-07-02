cbb6eddf78b5518a8f1ea1c481e9986c3fa231ec
Mohammad Hossein Ahmadi <mh.ahmadi258.work@gmail.com>
2020-07-02 18:18:29 +0430

    handle cities url bug

--- authapp/urls.py
+++ authapp/urls.py
@@ -8,5 +8,5 @@ app_name = 'authapp'
 urlpatterns = [
     path('create/', UserCreationView.as_view(), name='user creation'),
     path('login/', obtain_auth_token, name='login'),
-    path('all-cities', CityView.as_view(), name='all cities'),
+    path('all-cities/', CityView.as_view(), name='all cities'),
 ]
